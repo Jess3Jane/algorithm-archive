ac1afd14e546aa2b73b3b12b4a7afd0b58e7f1b8
Jess 3Jane <me@jess.coffee>
2018-06-30 19:53:09 -0400

    Implemented jarvis gift wrapping

--- chapters/computational_geometry/gift_wrapping/jarvis_march/code/rust/jarvis.rs
+++ chapters/computational_geometry/gift_wrapping/jarvis_march/code/rust/jarvis.rs
@@ -1,23 +1,9 @@
-use std::ops::{Add, Sub};
+use std::ops::Sub;
 
 #[derive(Debug, PartialEq)]
 struct Point {
     x: f64,
     y: f64,
-}
-
-impl Point {
-    fn cross(self, other: Point) -> f64 {
-        self.x * other.y - self.y * other.x
-    }
-}
-
-impl<'a> Add for &'a Point {
-    type Output = Point;
-
-    fn add(self, other: &'a Point) -> Point {
-        Point { x: self.x + other.x, y: self.y + other.y }
-    }
 }
 
 impl<'a> Sub for &'a Point {
@@ -28,21 +14,47 @@ impl<'a> Sub for &'a Point {
     }
 }
 
-fn jarvis_cross(p1: &Point, p2: &Point, p3: &Point) -> f64 {
-    let v1 = p2 - p1;
-    let v2 = p3 - p2;
-    let ret = Point::cross(v1, v2);
-    ret*ret
+fn is_left_of(a: &Point, b: &Point, p: &Point) -> bool {
+    let v1 = b - a;
+    let v2 = p - a;
+    v1.x * v2.y > v2.x * v1.y
+}
+
+fn jarvis_march<'a>(points: &'a[Point]) -> Vec<&'a Point> {
+    let mut hull = Vec::new();
+
+    // Start with the leftmost point
+    let mut point_on_hull = points.iter().fold(&points[0], |p1, p2| if p1.x < p2.x { p1 } else { p2 });
+
+    // Keep adding points until we reach the first point again
+    loop {
+        hull.push(point_on_hull);
+
+        point_on_hull = points.iter().fold(&points[0], |potential, current| {
+            if potential == point_on_hull || is_left_of(point_on_hull, potential, current) {
+                current
+            } else {
+                potential
+            }
+        });
+
+        if point_on_hull == hull[0] { break; } 
+    }
+
+    hull
 }
 
 fn main() {
-    let p1 = Point { x:  10.0, y:  10.0 };
-    let p2 = Point { x: -10.0, y: -10.0 };
+    let points = vec![
+        Point{ x: 1.0, y: 3.0 },
+        Point{ x: 2.0, y: 4.0 },
+        Point{ x: 4.0, y: 0.0 },
+        Point{ x: 1.0, y: 0.0 },
+        Point{ x: 0.0, y: 2.0 },
+        Point{ x: 2.0, y: 2.0 },
+        Point{ x: 3.0, y: 4.0 },
+        Point{ x: 3.0, y: 1.0 }];
 
-    println!("P1: {:?}", p1);
-    println!("P2: {:?}", p2);
-
-    println!("P1 + P2: {:?}", &p1 + &p2);
-    println!("P1 - P2: {:?}", &p1 - &p2);
-    println!("P1 x P2: {}", p1.cross(p2));
+    let hull = jarvis_march(&points);
+    println!("{:?}", hull);
 }
